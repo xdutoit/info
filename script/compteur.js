128a9500bc67663194399e08c7dd2dc11d550469
xdutoit <67094208+xdutoit@users.noreply.github.com>
2020-11-21 22:20:59 +0100

    2.0.2 - compteur flexible et commentaires JS
    ajout de la variable counter_size, de commentaires, et de var et ; (trop l'habitude de Python...)

--- script/compteur.js
+++ script/compteur.js
@@ -6,12 +6,17 @@ $(function(){
 })
 
 radix_list = {'dec':10,'bin':2,'hex':16}
+counter_size = 4
+
+/* principe de base: tout est converti en décimal, puis modifié, puis retourné dans la base d'origine */
 
 function getBase(obj){
-  return $(obj).parents('.div_compteur_ext').attr('id')
+  // retourne 'dec', 'bin' ou 'hex'
+  return $(obj).parents('.div_compteur_ext').attr('id');
 }
 
 function changeVal(e){
+  // change la valeur du compteur (this) de (e.data)
   var base = getBase(this);
   var num = getDecValue(base);
   num += parseInt(e.data);
@@ -20,37 +25,32 @@ function changeVal(e){
 }
 
 function getDecValue(base){
+  // retourne l'équivalent décimal du compteur 'base'
   var num_str = '';
-  for(var i=3;i>=0;i--){
+  for(var i=counter_size-1;i>=0;i--){
     num_str += $('#'+base+' .n'+i).val()
   }
   return parseInt(num_str,radix_list[base]);
 }
 
 function setDecValue(n,base){
+  // donne la valeur décimale n au compteur 'base'
   if(n<0){
     n=0;
   }
-  n_str = n.toString(radix_list[base]);
-  l = n_str.length
-  for(var i=0;i<4;i++){
-    v = 0
+  var n_str = n.toString(radix_list[base]);
+  var l = n_str.length;
+  for(var i=0;i<counter_size;i++){
+    var v = 0;
     if(i<l){
-      v = n_str[l-i-1].toUpperCase()
+      v = n_str[l-i-1].toUpperCase();
     }
     $('#'+base+' .n'+i).val(v);
   }
 }
 
-
 function reset(){
+  // remet à zéro le compteur 'this'
   var base = getBase(this);
   setDecValue(0,base);
 }
-
-/*
-parseInt('101',2) -> 5 bin2dec
-
-dec2bin:
-
-(54).toString(2)*/
