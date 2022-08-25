c90b5dccdd1c871acdabfca633e184360fcaad91
xdutoit <67094208+xdutoit@users.noreply.github.com>
2022-08-25 15:29:14 +0200

    xls2sql v2
    possibilité de choisir les types (numérique ou non) de chaque colonne

--- script/xls2sql.js
+++ script/xls2sql.js
@@ -1,17 +1,19 @@
 // no jQuery
 document.addEventListener("DOMContentLoaded", function(event) 
 {
-	document.querySelector('#btn_convert').addEventListener('click',convert);
-})
+	document.querySelector('#btn_convert_auto').addEventListener('click',convert_autodetect);
+	document.querySelector('#btn_convert_man').addEventListener('click',convert_manual);
+    document.querySelector('#ta_in').addEventListener('change',update_cols);
+});
 
-function convert(){
+function txt2array(){
     let txt_in = document.querySelector('#ta_in').value;
-    
+
     // split \n
     let lines_0 = txt_in.split('\n');
 
     // trim and split \t
-    let lines = []
+    lines = []
     for(let i=0;i<lines_0.length;i++){
         let l = lines_0[i].split('\t');
         let found = false;
@@ -27,7 +29,7 @@ function convert(){
     }
 
     // check number of columns
-    let n_cols = lines[0].length;
+    n_cols = lines[0].length;
     for(let i=1;i<lines.length;i++){
         if(lines[i].length != n_cols){
             alert('Le nombres de colonnes n\'est pas cohérents.\n(certaines lignes ont '+n_cols+' colonnes et d\'autres en ont '+lines[i].length+').');
@@ -35,17 +37,71 @@ function convert(){
         }
     }
 
+    return lines;
+}
+
+function update_cols(types){
+    // crée le tableau avec les checkboxes pour choisir si une colonne est numérique ou non
+    // si types est passé en paramètre, les checkboxes sont pré-remplis
+
+    document.getElementById('tr_colnums').innerHTML = '';
+    document.getElementById('tr_inputs').innerHTML = '';
+
+    types = types || [];
+    let lines = txt2array();
+    let n_cols = lines[0].length;
+
+    for(let c=0;c<n_cols;c++){
+        let chk = document.createElement('input');
+        chk.setAttribute('id','chk_'+c);
+        chk.setAttribute('value',1);
+        chk.setAttribute('type','checkbox');
+        if(c<types.length && types[c]){
+            chk.checked = 'checked';
+        }
+        let td1 = document.createElement('td');
+        td1.appendChild(document.createTextNode((c+1)));
+        let td2 = document.createElement('td');
+        td2.appendChild(chk);
+
+        document.getElementById('tr_colnums').appendChild(td1);
+        document.getElementById('tr_inputs').appendChild(td2);
+    }
+}
+
+function convert_manual(){
+    convert(false);
+}
+
+function convert_autodetect(){
+    convert(true);
+}
+
+function convert(auto){
+    let lines = txt2array();
+    let n_cols = lines[0].length;
+
     // check types
     let types = [];
-    for(let c=0;c<n_cols;c++){
-        let t=1; // 0=txt / 1=num
-        for(let i=0;i<lines.length;i++){
-            if(!strIsNum(lines[i][c])){
-                t=0;
+    if(auto){
+        // auto-detect types
+        for(let c=0;c<n_cols;c++){
+            let t=1; // 0=txt / 1=num
+            for(let i=0;i<lines.length;i++){
+                if(!strIsNum(lines[i][c])){
+                    t=0;
+                }
             }
+            types.push(t);
         }
-        types.push(t);
+        update_cols(types);
     }
+    else{
+        for(let c=0;c<n_cols;c++){
+            types.push(document.getElementById('chk_'+c).checked?1:0);
+        }
+    }
+    
     
     // output
     let str_out = '';
@@ -63,7 +119,10 @@ function convert(){
         }
         str_out += ')';
         if(i<lines.length-1){
-            str_out += ',\n'
+            str_out += ',\n';
+        }
+        else{
+            str_out += ';';
         }
     }
     document.querySelector('#ta_out').value = str_out;
@@ -71,14 +130,9 @@ function convert(){
 
 function strIsNum(str_in){
     if(isNaN(parseFloat(str_in))){
-        console.log(str_in + 'not num')
         return false;
     }
     else{
         return true;
-        // TODO
-        // date ?
-        
-
     }
 }
