8442a6ce729a5c1ddc0a2f88f4092a07335fcc2d
xdutoit <67094208+xdutoit@users.noreply.github.com>
2022-09-29 15:28:41 +0200

    update xls2sql
    ajout de la prise en compte de NULL

--- script/xls2sql.js
+++ script/xls2sql.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function(event)
 
 let lines;
 
+const NULL = -1;
 const NUM = 0;
 const TXT = 1;
 const DATE = 2;
@@ -19,7 +20,18 @@ function parseLines(){
     let types = [];
 
     for(let c=0;c<n_cols;c++){
-        types.push(detectType(lines[0][c]));
+        let l=0;
+        let t = detectType(lines[l][c]);
+        console.log('val:'+lines[l][c]+' / type:'+t);
+        while(t==NULL){
+            l++;
+            if(l==lines.length){
+                alert('il y a une colonne vide');
+                return false;
+            }
+            t = detectType(lines[l][c]);
+        }
+        types.push(t);
     }
 
     // remplit le tableau
@@ -61,7 +73,10 @@ function parseLines(){
 }
 
 function detectType(data_in){
-    let data_num = parseFloat(data_in)
+    let data_num = parseFloat(data_in);
+    if (data_in==''){
+        return NULL;
+    }
     if(!isNaN(data_num) && data_num+'' == data_in){
         return NUM;
     }
@@ -150,18 +165,23 @@ function convert(){
                 }
             }
 
-
-
             let quote = '\'';
             if(type==NUM){
                 quote = '';
             }
             let data = lines[i][c];
-            if(type==DATE){
-                data = convertDate(data);
+            if(data==''){
+                data = 'null';
+                quote = '';
             }
-            if(type==TXT){
-                data = data.replace(/'/g,"''");
+            else{
+                if(type==DATE){
+                    data = convertDate(data);
+                }
+                if(type==TXT){
+                    data = data.replace(/'/g,"''");
+                }
+
             }
             str_out += sep+quote+data+quote;
             sep = ', ';
@@ -176,100 +196,3 @@ function convert(){
     }
     document.querySelector('#ta_out').value = str_out;
 }
-/*
-function update_cols(types){
-    // crée le tableau avec les checkboxes pour choisir si une colonne est numérique ou non
-    // si types est passé en paramètre, les checkboxes sont pré-remplis
-
-    document.getElementById('tr_colnums').innerHTML = '';
-    document.getElementById('tr_inputs').innerHTML = '';
-
-    types = types || [];
-    let lines = txt2array();
-    let n_cols = lines[0].length;
-
-    for(let c=0;c<n_cols;c++){
-        let chk = document.createElement('input');
-        chk.setAttribute('id','chk_'+c);
-        chk.setAttribute('value',1);
-        chk.setAttribute('type','checkbox');
-        if(c<types.length && types[c]){
-            chk.checked = 'checked';
-        }
-        let td1 = document.createElement('td');
-        td1.appendChild(document.createTextNode((c+1)));
-        let td2 = document.createElement('td');
-        td2.appendChild(chk);
-
-        document.getElementById('tr_colnums').appendChild(td1);
-        document.getElementById('tr_inputs').appendChild(td2);
-    }
-}
-
-function convert_manual(){
-    convert(false);
-}
-
-function convert_autodetect(){
-    convert(true);
-}
-
-function convert(auto){
-    let lines = txt2array();
-    let n_cols = lines[0].length;
-
-    // check types
-    let types = [];
-    if(auto){
-        // auto-detect types
-        for(let c=0;c<n_cols;c++){
-            let t=1; // 0=txt / 1=num
-            for(let i=0;i<lines.length;i++){
-                if(!strIsNum(lines[i][c])){
-                    t=0;
-                }
-            }
-            types.push(t);
-        }
-        update_cols(types);
-    }
-    else{
-        for(let c=0;c<n_cols;c++){
-            types.push(document.getElementById('chk_'+c).checked?1:0);
-        }
-    }
-    
-    
-    // output
-    let str_out = '';
-    for(let i=0;i<lines.length;i++){
-        str_out += '(';
-
-        let sep = '';
-        for(let c=0;c<n_cols;c++){
-            let quote = '\'';
-            if(types[c]){
-                quote = '';
-            }
-            str_out += sep+quote+lines[i][c]+quote;
-            sep = ', ';
-        }
-        str_out += ')';
-        if(i<lines.length-1){
-            str_out += ',\n';
-        }
-        else{
-            str_out += ';';
-        }
-    }
-    document.querySelector('#ta_out').value = str_out;
-}
-
-function strIsNum(str_in){
-    if(isNaN(parseFloat(str_in))){
-        return false;
-    }
-    else{
-        return true;
-    }
-}*/
